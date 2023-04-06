4cb3ecf639be7b60fa1b081508e2c18567ee1445
lotos441 <lotosnl036@gmail.com>
2023-04-06 17:33:56 +0500

    commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
 import Lessons from './components/Lessons/Lessons';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-  Link,
-} from 'react-router-dom';
+
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
-        <Link className='group_link' to="/lessons">3ПИ</Link>
-        <Routes>
-          <Route path="/lessons" element={<Lessons/>}></Route>
-        </Routes>
+          <Lessons/>
       </div>
-    </BrowserRouter>
   );
 }
 
