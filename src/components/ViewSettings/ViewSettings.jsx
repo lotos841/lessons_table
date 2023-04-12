40c4c2a5728552f655056c117b0907ce5c077039
lotos441 <lotosnl036@gmail.com>
2023-04-12 12:55:46 +0500

    ViewSettings layout

--- src/components/ViewSettings/ViewSettings.jsx
+++ src/components/ViewSettings/ViewSettings.jsx
@@ -12,6 +12,8 @@ const Settings = () => {
                 <ViewBlock setting_title="задний фон" root_variable="--main-bg-color"/>
                 <ViewBlock setting_title="цвет блоков" root_variable="--block-bg-color"/>
                 <ViewBlock setting_title="цвет текста" root_variable="--text-color"/>
+                <ViewBlock setting_title="цвет рамок" root_variable="--border-color"/>
+                <ViewBlock setting_title="цвет hover(наведение)" root_variable="--hover-color"/>
                 <p>ps. Пока нет куки файлов для запоминания</p>
             </div>
         </div>
